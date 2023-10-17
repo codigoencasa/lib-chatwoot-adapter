90b8775f7df44ac484a1ed595da5cd26f92b1bf2
Carlos Moran <bl4ckdll@gmail.com>
2023-10-17 02:03:08 -0500

    feat: � Mejoras en ChatwootClient

--- Services/class/botWrapper.js
+++ Services/class/botWrapper.js
@@ -2,11 +2,16 @@ const Queue = require("queue-promise");
 const ChatwootClient = require("./chatwoot");
 
 class BotWrapper {
+  // Instancia del bot.
   static botInstance = null;
+
+  // Cola para gestionar eventos de forma secuencial.
   static queue = new Queue({
     concurrent: 1,
     interval: 500,
   });
+
+  // Instancia del cliente de Chatwoot.
   static chatwoot;
 
   /**
@@ -32,8 +37,22 @@ class BotWrapper {
       this.botInstance.providerClass.on("message", (data) => {
         this.queue.enqueue(async () => {
           try {
-            const { from, body } = data;
-            this.chatwoot.sendNotes(from, body, "incoming", false);
+            const { from, body, pushName, url } = data;
+            const token = this.botInstance.providerClass.jwtToken;
+
+            if (data.body.includes("_event_")) {
+              await this.chatwoot.sendMessageWithAttachments(
+                from,
+                body,
+                [url],
+                "incoming",
+                false,
+                token
+              );
+            } else {
+              // Envía el mensaje al cliente de Chatwoot.
+              this.chatwoot.sendNotes(from, body, "incoming", false, pushName);
+            }
           } catch (error) {
             console.error("Error al procesar send_message:", error);
           }
@@ -46,10 +65,28 @@ class BotWrapper {
       this.botInstance.on("send_message", (data) => {
         this.queue.enqueue(async () => {
           try {
-            // Extrae datos del mensaje.
-            const { numberOrId, answer } = data;
-            // Envía notas a Chatwoot.
-            this.chatwoot.sendNotes(numberOrId, answer, "outgoing", false);
+            const { numberOrId, answer, ctxMessage } = data;
+
+            // Verifica si existe una URL de imagen o documento en ctxMessage.options.media
+            const Url_Adjunto = ctxMessage?.options?.media;
+
+            if (Url_Adjunto) {
+              await this.chatwoot.sendMessageWithAttachments(
+                numberOrId,
+                answer,
+                [Url_Adjunto],
+                "outgoing",
+                false
+              );
+            } else {
+              // Envía el mensaje saliente al cliente de Chatwoot.
+              await this.chatwoot.sendNotes(
+                numberOrId,
+                answer,
+                "outgoing",
+                false
+              );
+            }
           } catch (error) {
             console.error("Error al procesar send_message:", error);
           }
